65364ec4f9499af1b4f4388135def2bddf41b4b2
Isa Gómez <hello@isagomez.com>
2021-06-20 12:07:46 +0200

    Añadimos el provider de react-redux a la app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/app';
 import { configureClient } from './api/client';
@@ -22,9 +23,11 @@ const store = configureStore({
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App isInitiallyLogged={!!accessToken} />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
